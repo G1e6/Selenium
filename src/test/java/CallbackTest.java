b8288bef271da892a9625ff43e85ba94e62b0774
G1e6 <gleb-potapov@rambler.ru>
2022-08-18 15:39:15 +0500

    Ferst commit

---
+++ src/test/java/CallbackTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CallbackTest {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+// убедитесь, что файл chromedriver.exe расположен именно в каталоге C:\tmp
+        System.setProperty("webdriver.chrome.driver", "./webdriver/chromedriver");
+    }
+
+    @BeforeEach
+    void setUp() {
+
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTestSomething() {
+        driver.get("http://localhost:9999/");
+    }
+}
